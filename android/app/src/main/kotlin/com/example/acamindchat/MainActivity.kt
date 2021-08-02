3b0d9c1d244b0d35ad98f572ed10d5c69d8274bc
ThatzSayem <thatzsayemchowdhury@gmail.com>
2021-08-02 07:12:24 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/acamindchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acamindchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+
+  final void Function(File pickedImage) imagePickFn;
+
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  late File _pickedImage;
+
+  void _pickImage(ImagePicker) async {
+    final pickedImageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+      maxHeight: 150,
+      preferredCameraDevice: CameraDevice.rear,
+    );
+    setState(() {
+      _pickedImage = pickedImageFile as File;
+    });
+    widget.imagePickFn(pickedImageFile as File);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          onPressed: _pickImage.,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+        ),
+      ],
+    );
+  }
+}
